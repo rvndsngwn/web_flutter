df575988ba4a7d046738548c6343c97460d10594
Maheshwar Ravuri <mravuri96@gmail.com>
2020-03-20 20:40:47 -0700

    Fixed WebView Refresh on scroll

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2,7 +2,7 @@
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "bf8c982fee5e76959d5efb85310ed1d1",
-"main.dart.js": "48b807492b4d42bc91e8a8c47e9b6e64",
+"main.dart.js": "c4e769ef428d983232ca20a729b5aa6a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
